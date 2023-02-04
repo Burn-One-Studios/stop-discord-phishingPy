52ea597f87d4a55a33169410e150dfc9b3844cfa
Kings <Kingsage311@gmail.com>
2023-02-04 08:37:54 -0800

    Update init for better calling

--- stopPhishing/__init__.py
+++ stopPhishing/__init__.py
@@ -1,9 +1,17 @@
-from stopPhishing.functions import check, list, amount
+from stopPhishing.functions.amount import allDomainCount, phishingDomainCount, suspiciousDomainCount
+from stopPhishing.functions.check import checkDomain, checkMessage, susDomainsChecker
+from stopPhishing.functions.list import listAllDomains, listPhishingDomains, listSuspiciousDomains
 
 __version__ = '0.1.0'
 
 __all__ = [
-    'check',
-    'list',
-    'amount'
+    'allDomainCount',
+    'checkDomain',
+    'checkMessage',
+    'listAllDomains',
+    'listPhishingDomains',
+    'listSuspiciousDomains',
+    'phishingDomainCount',
+    'suspiciousDomainCount',
+    'susDomainsChecker',
 ]
